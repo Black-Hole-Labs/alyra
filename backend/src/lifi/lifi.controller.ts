c8f024fa95daac97113589b2278478e9730221e8
rpoltavets <rpoltavez@gmail.com>
2024-12-15 11:53:47 +0200

    implement main getters via LIFI api

--- backend/src/lifi/lifi.controller.ts
+++ backend/src/lifi/lifi.controller.ts
@@ -1,7 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { LifiService } from './lifi.service';
 
 @Controller('lifi')
 export class LifiController {
   constructor(private readonly lifiService: LifiService) {}
+
+  @Get('get-chains')
+  async getChains() {
+    return await this.lifiService.getChains();
+  }
+
+  @Get('get-tools')
+  async getTools() {
+    return await this.lifiService.getTools();
+  }
+
+  @Get('get-tokens')
+  async getTokens() {
+    return await this.lifiService.getTokens();
+  }
 }
--- backend/src/lifi/lifi.service.ts
+++ backend/src/lifi/lifi.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class LifiService {}
+export class LifiService {
+
+  async getChains() {
+    /// chainTypes can be of type SVM and EVM, by default, only EVM chains will be returned
+    const optionalChainTypes = "EVM"
+    const result = await axios.get('https://li.quest/v1/chains', 
+      {params: {chainTypes: optionalChainTypes}});
+    return result.data;
+  }
+
+  async getTools() {
+    const result = await axios.get('https://li.quest/v1/tools');
+    return result.data;
+  }
+
+  async getTokens() {
+    const optionalFilter = ['ETH', 137] // Both numeric and mnemonic can be used
+    /// chainTypes can be of type SVM and EVM. By default, only EVM tokens will be returned
+    const optionalChainTypes = "EVM"
+    const result = await axios.get('https://li.quest/v1/tokens',
+        {params: {
+            chains: optionalFilter.join(','),
+            chainTypes: optionalChainTypes 
+        }});
+    return result.data;
+  }
+}
