c81167bdd6a208210a45a3e450ca6cd5f1ae767c
rpoltavets <rpoltavez@gmail.com>
2024-12-24 17:11:46 +0200

    fix show tokens

--- frontend/src/app/components/popup/token-change/token-change.component.ts
+++ frontend/src/app/components/popup/token-change/token-change.component.ts
@@ -15,26 +15,14 @@ export class TokenChangePopupComponent {
   @Output() tokenSelected = new EventEmitter<{ symbol: string; imageUrl: string }>();
 
   searchText: string = '';
-  tokens: { symbol: string; name: string; contractAddress: string; imageUrl: string }[] = [];
-  filteredTokens = [...this.tokens];
-  private blockchainStateService = inject(BlockchainStateService);
-
-  constructor() {
-    effect(() => {
-      const network = this.blockchainStateService.network();
-      console.log(network);
-      if (network) {
-        this.loadTokensForNetwork(network);
-      }
-    });
-  }
+  public blockchainStateService = inject(BlockchainStateService);
 
   ngOnInit(): void {
   }
 
   performSearch(): void {
     const search = this.searchText.toLowerCase().trim();
-    this.filteredTokens = this.tokens.filter(
+    this.blockchainStateService.filteredTokens = this.blockchainStateService.tokens.filter(
       token =>
         token.name.toLowerCase().includes(search) ||
         token.symbol.toLowerCase().includes(search) ||
@@ -56,37 +44,5 @@ export class TokenChangePopupComponent {
     navigator.clipboard.writeText(address).catch(() => {
       console.error('Failed to copy to clipboard');
     });
-  }
-
-  private loadTokensForNetwork(network: string): void {
-    fetch(`/data/tokens.json`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to load tokens for network ${network}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        const tokensForNetwork = data.tokensEVM[network];
-        if (tokensForNetwork) {
-          // console.log(tokensForNetwork);
-          this.tokens = tokensForNetwork.map((token: any) => ({
-            symbol: token.symbol,
-            name: token.name,
-            contractAddress: token.address,
-            imageUrl: token.logoURI,
-          }));
-          this.filteredTokens = [...this.tokens];
-        } else {
-          console.warn(`No tokens found for network ${network}`);
-          this.tokens = [];
-          this.filteredTokens = [];
-        }
-      })
-      .catch((error) => {
-        console.error(`Error loading tokens: ${error.message}`);
-        this.tokens = [];
-        this.filteredTokens = [];
-      });
   }
 }
--- frontend/src/app/services/blockchain-state.service.ts
+++ frontend/src/app/services/blockchain-state.service.ts
@@ -6,12 +6,52 @@ export class BlockchainStateService {
   readonly provider = signal<string | null>(null); // Выбранная сеть
   readonly network = signal<string | null>(null); // Выбранная сеть
   readonly connected = signal<boolean>(false); // Статус подключения
+  tokens: { symbol: string; name: string; contractAddress: string; imageUrl: string }[] = [];
+  filteredTokens = [...this.tokens];
 
   constructor() {
     // Используем effect для автоматического обновления connected
     effect(() => {
       this.connected.set(this.walletAddress() !== null);
     },{ allowSignalWrites: true });
+
+    effect(() => {
+      if (this.network()) {
+        this.loadTokensForNetwork(this.network()!);
+      }
+    });
+  }
+
+  private loadTokensForNetwork(network: string): void {
+    fetch(`/data/tokens.json`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to load tokens for network ${network}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const tokensForNetwork = data.tokensEVM[network];
+        if (tokensForNetwork) {
+          // console.log(tokensForNetwork);
+          this.tokens = tokensForNetwork.map((token: any) => ({
+            symbol: token.symbol,
+            name: token.name,
+            contractAddress: token.address,
+            imageUrl: token.logoURI,
+          }));
+          this.filteredTokens = [...this.tokens];
+        } else {
+          console.warn(`No tokens found for network ${network}`);
+          this.tokens = [];
+          this.filteredTokens = [];
+        }
+      })
+      .catch((error) => {
+        console.error(`Error loading tokens: ${error.message}`);
+        this.tokens = [];
+        this.filteredTokens = [];
+      });
   }
 
   updateWalletAddress(address: string | null): void {
