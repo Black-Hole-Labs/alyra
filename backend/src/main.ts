ba05713325a7e26728a6f78a2047d0b55786c846
artem.chornyi <artem.chornyi@listat.com.ua>
2025-03-16 13:34:13 +0100

    env back

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://black-hole-front.onrender.com',
+    origin: process.env.ORIGIN || 'http://localhost:4200',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
   });
