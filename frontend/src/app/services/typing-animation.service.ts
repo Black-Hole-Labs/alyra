b34d71d45e7d1fc42cee2b028f6edb7093df45dd
rpoltavets <rpoltavez@gmail.com>
2025-05-26 16:20:53 +0300

    remove redundant code

--- frontend/src/app/services/typing-animation.service.ts
+++ frontend/src/app/services/typing-animation.service.ts
@@ -5,7 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class TypingAnimationService {
   private readonly DEFAULT_DELAY = 50;
-  private readonly DEFAULT_CURSOR_BLINK_DELAY = 500;
 
   constructor() {}
 
@@ -47,42 +46,6 @@ export class TypingAnimationService {
 
     if (cursorElement) {
       cursorElement.remove();
-    }
-
-    if (onComplete) {
-      onComplete();
-    }
-  }
-
-  async typeMultipleLines(
-    elements: HTMLElement[],
-    texts: string[],
-    options: {
-      delay?: number;
-      lineDelay?: number;
-      cursor?: boolean;
-      cursorChar?: string;
-      onComplete?: () => void;
-    } = {}
-  ): Promise<void> {
-    const {
-      delay = this.DEFAULT_DELAY,
-      lineDelay = 200,
-      cursor = true,
-      cursorChar = '|',
-      onComplete
-    } = options;
-
-    for (let i = 0; i < elements.length; i++) {
-      await this.typeText(elements[i], texts[i], {
-        delay,
-        cursor,
-        cursorChar
-      });
-      
-      if (i < elements.length - 1) {
-        await new Promise(resolve => setTimeout(resolve, lineDelay));
-      }
     }
 
     if (onComplete) {
