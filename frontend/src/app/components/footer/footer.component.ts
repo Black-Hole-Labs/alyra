5bd1d3066b0579f519ec65b300d6890694bfb2e5
esempe <steam.dota.return@gmail.com>
2025-05-27 20:53:58 +0200

    remove magic numbers

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, OnDestroy, effect, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, effect, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ethers } from 'ethers';
-import { TokenService } from '../../services/token.service';
-import { BlockchainStateService } from '../../services/blockchain-state.service';
 import { Connection } from '@solana/web3.js';
+import { ethers } from 'ethers';
+import { BlockchainStateService } from '../../services/blockchain-state.service';
+import { TokenService } from '../../services/token.service';
 
 @Component({
   selector: 'app-footer',
@@ -17,13 +17,15 @@ export class FooterComponent implements OnInit, OnDestroy {
   private providerEvm = signal<ethers.JsonRpcProvider | undefined>(undefined);
   private providerSol = signal<Connection | undefined>(undefined);
   private updateInterval: any;
+  private SOL_CHAIN_ID = 1151111081099710;
+  private EVM_CHAIN_ID = 1;
 
   constructor(
     private tokenService: TokenService,
     private blockchainStateService: BlockchainStateService,
   ) {
-    const rpcUrlEvm = this.blockchainStateService.getNetworkById(1)?.rpcUrls[0];
-    const rpcUrlSol = this.blockchainStateService.getNetworkById(1151111081099710)?.rpcUrls[0];
+    const rpcUrlEvm = this.blockchainStateService.getNetworkById(this.EVM_CHAIN_ID)?.rpcUrls[0];
+    const rpcUrlSol = this.blockchainStateService.getNetworkById(this.SOL_CHAIN_ID)?.rpcUrls[0];
     this.providerEvm.set(new ethers.JsonRpcProvider(rpcUrlEvm));
     this.providerSol.set(new Connection(rpcUrlSol!));
 
@@ -33,8 +35,7 @@ export class FooterComponent implements OnInit, OnDestroy {
         if (!token) return;
 
         const network = this.blockchainStateService.getNetworkById(token.chainId);
-        if (network?.rpcUrls[0] && token.chainId !== 1151111081099710) {
-          console.log('network', network);
+        if (network?.rpcUrls[0] && token.chainId !== this.SOL_CHAIN_ID) {
           this.providerEvm.set(new ethers.JsonRpcProvider(network.rpcUrls[0]));
           this.updateBlockNumber();
         }
@@ -63,7 +64,7 @@ export class FooterComponent implements OnInit, OnDestroy {
       const token = this.tokenService.getSelectedToken();
       if (!token) return;
 
-      if (token.chainId === 1151111081099710) {
+      if (token.chainId === this.SOL_CHAIN_ID) {
         const slot = await this.providerSol()?.getSlot();
         this.block.set(slot ?? 0);
       } else {
