7c42377b51238da94eba9026c79179f796806335
esempe <steam.dota.return@gmail.com>
2025-05-27 20:58:57 +0200

    remove magic number v2

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -4,6 +4,7 @@ import { Connection } from '@solana/web3.js';
 import { ethers } from 'ethers';
 import { BlockchainStateService } from '../../services/blockchain-state.service';
 import { TokenService } from '../../services/token.service';
+import { NetworkId } from '../../models/wallet-provider.interface';
 
 @Component({
   selector: 'app-footer',
@@ -17,15 +18,13 @@ export class FooterComponent implements OnInit, OnDestroy {
   private providerEvm = signal<ethers.JsonRpcProvider | undefined>(undefined);
   private providerSol = signal<Connection | undefined>(undefined);
   private updateInterval: any;
-  private SOL_CHAIN_ID = 1151111081099710;
-  private EVM_CHAIN_ID = 1;
 
   constructor(
     private tokenService: TokenService,
     private blockchainStateService: BlockchainStateService,
   ) {
-    const rpcUrlEvm = this.blockchainStateService.getNetworkById(this.EVM_CHAIN_ID)?.rpcUrls[0];
-    const rpcUrlSol = this.blockchainStateService.getNetworkById(this.SOL_CHAIN_ID)?.rpcUrls[0];
+    const rpcUrlEvm = this.blockchainStateService.getNetworkById(NetworkId.ETHEREUM_MAINNET)?.rpcUrls[0];
+    const rpcUrlSol = this.blockchainStateService.getNetworkById(NetworkId.SOLANA_MAINNET)?.rpcUrls[0];
     this.providerEvm.set(new ethers.JsonRpcProvider(rpcUrlEvm));
     this.providerSol.set(new Connection(rpcUrlSol!));
 
@@ -35,7 +34,7 @@ export class FooterComponent implements OnInit, OnDestroy {
         if (!token) return;
 
         const network = this.blockchainStateService.getNetworkById(token.chainId);
-        if (network?.rpcUrls[0] && token.chainId !== this.SOL_CHAIN_ID) {
+        if (network?.rpcUrls[0] && token.chainId !== NetworkId.SOLANA_MAINNET) {
           this.providerEvm.set(new ethers.JsonRpcProvider(network.rpcUrls[0]));
           this.updateBlockNumber();
         }
@@ -64,7 +63,7 @@ export class FooterComponent implements OnInit, OnDestroy {
       const token = this.tokenService.getSelectedToken();
       if (!token) return;
 
-      if (token.chainId === this.SOL_CHAIN_ID) {
+      if (token.chainId === NetworkId.SOLANA_MAINNET) {
         const slot = await this.providerSol()?.getSlot();
         this.block.set(slot ?? 0);
       } else {
