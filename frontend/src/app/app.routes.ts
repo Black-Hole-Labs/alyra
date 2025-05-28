eef06a14dc66ff2f6377170967088baa19498b5b
rpoltavets <rpoltavez@gmail.com>
2025-05-28 23:19:18 +0300

    remove /trade from routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { QuestsComponent } from './pages/quests/quests.component';
 
 export const routes: Routes = [
   { path: '', component: TradeComponent, data: { title: 'Trade' } },
-  { path: 'trade', component: TradeComponent, data: { title: 'Trade' } },
+  // { path: 'trade', component: TradeComponent, data: { title: 'Trade' } },
   { path: 'rewards', component: QuestsComponent, data: { title: 'Rewards' } },
 ];
 
