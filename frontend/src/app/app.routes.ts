ce341e88f484e809966db7180d27bd7e23362e0f
kerrnney <kerrnney@gmail.com>
2025-06-30 18:45:16 +0300

    - quests is rewards

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TradeComponent } from './pages/trade/trade.component';
-import { QuestsComponent } from './pages/quests/quests.component';
+import { RewardsComponent } from './pages/rewards/rewards.component';
 import { FormPageComponent } from './pages/form-page/form-page.component';
 import { DocumentationPageComponent } from './components/documentation/documentation-page/documentation-page.component';
 import { documentationRoutes } from './components/documentation/documentation.routes';
@@ -9,7 +9,7 @@ import { documentationRoutes } from './components/documentation/documentation.ro
 export const routes: Routes = [
   { path: '', component: TradeComponent, data: { title: 'Trade' } },
   // { path: 'trade', component: TradeComponent, data: { title: 'Trade' } },
-  { path: 'rewards', component: QuestsComponent, data: { title: 'Rewards' } },
+  { path: 'rewards', component: RewardsComponent, data: { title: 'Rewards' } },
   { path: 'pro', component: FormPageComponent, data: { title: 'Pro' } },
 	{
     path: 'documentation',
