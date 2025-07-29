2d97587c30009c12f49c301f4d1c5af6525fa034
artem.chornyi <artem.chornyi@listat.com.ua>
2025-07-29 22:12:17 +0200

    icp

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,8 +8,19 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    process.env.ORIGIN || 'http://localhost:4200',
+    'https://xwfnn-qiaaa-aaaad-qhoeq-cai.icp0.io',
+  ];
+
   app.enableCors({
-    origin: process.env.ORIGIN || 'http://localhost:4200',
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
