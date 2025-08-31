df34395edbf90bc0e17754caca6aa75e6fb6946e
rpolt <rpoltavez@gmail.com>
2025-08-31 20:37:23 +0300

    fix link

--- frontend/src/environments/environment.stage.ts
+++ frontend/src/environments/environment.stage.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dev-back-black-hole.onrender.com'
+  apiUrl: 'https://dev-back-black-hole.onrender.com',
+  apiBack: 'https://api.blackhole.build' // TODO: make dev-version for API
 };
